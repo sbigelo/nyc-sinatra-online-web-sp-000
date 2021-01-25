65fd998140c189c369cbf8380f152d52e921d782
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-25 18:03:13 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,7 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :'/landmarks/index'
+  end
 end
