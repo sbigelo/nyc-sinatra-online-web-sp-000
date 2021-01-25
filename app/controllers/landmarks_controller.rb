a2b80cc684f6910f983820fed052f8b0ec0b21f5
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-25 22:36:44 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -30,7 +30,7 @@ class LandmarksController < ApplicationController
   post '/landmarks' do
     Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
     redirect '/landmarks'
-    end
   end
+
 
 end
