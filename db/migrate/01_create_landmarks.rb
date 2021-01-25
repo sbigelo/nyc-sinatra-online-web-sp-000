a74c6a9532cdf982e38b5ae23d4aa79a288e99a4
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-25 20:34:12 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -3,8 +3,6 @@ class CreateLandmarks < ActiveRecord::Migration
   def change
     create_table :landmarks do |t|
     t.string :name
-    t.integer :figure_id
-    t.integer :year_completed
     end
   end
 
